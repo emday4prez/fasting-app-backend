69120560b079c64ac82606fc0d9c0d6c63436300
Emerson <emerson.day@gmail.com>
2022-07-23 16:19:06 -0500

    updated gitignore, added build

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -12,6 +13,7 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
+
 
 
 morgan.token('person', (req, res) => JSON.stringify(req.body))
